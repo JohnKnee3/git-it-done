d1b50fec01d49e550d725de1b00536989fbda76d
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-20 19:15:17 -0700

    6.5 zip squish

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,8 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -23,12 +23,12 @@ var formSubmitHandler = function(event) {
 };
 
 var buttonClickHandler = function(event) {
-  var language = event.target.getAttribute("data-language")
-  console.log(language);
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
 
   if (language) {
     getFeaturedRepos(language);
-  
+
     // clear old content
     repoContainerEl.textContent = "";
   }
@@ -58,16 +58,18 @@ var getUserRepos = function(user) {
 };
 
 var getFeaturedRepos = function(language) {
+  // format the github api url
   var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
+  // make a get request to url
   fetch(apiUrl).then(function(response) {
+    // request was successful
     if (response.ok) {
       response.json().then(function(data) {
         displayRepos(data.items, language);
-        console.log(data);
       });
     } else {
-      alert('Error: GitHub User Not Found');
+      alert("Error: " + response.statusText);
     }
   });
 };
@@ -118,7 +120,6 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
-// add event listeners to forms
+// add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
-
 languageButtonsEl.addEventListener("click", buttonClickHandler);
